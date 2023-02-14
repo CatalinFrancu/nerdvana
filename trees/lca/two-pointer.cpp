7d83b5aada74f67b8a29efd81e47b349f007ba57
Cătălin Frâncu <cata@francu.com>
2023-02-14 13:02:51 +0200

    Encapsulate data in the fastest version. 10% faster now.

--- trees/lca/two-pointer.cpp
+++ trees/lca/two-pointer.cpp
@@ -9,71 +9,74 @@
 struct edge {
   int v, next;
 } e[2 * MAX_N];
-int adj[MAX_N];   // adjacency lists
-int p[MAX_N];     // parents
-int d[MAX_N];     // depths
-int jump[MAX_N];  // jump pointers
+
+struct node {
+  int adj;   // adjacency list
+  int p;     // parent
+  int d;     // depth
+  int jump;  // jump pointer
+} n[MAX_N];
 
 void add_edge(int u, int v, int pos) {
-  e[pos] = { v, adj[u] };
-  adj[u] = pos;
+  e[pos] = { v, n[u].adj };
+  n[u].adj = pos;
 }
 
 // Traverse the tree and compute parents, depths and jump pointers.
 void dfs(int u) {
 
-  int u2 = jump[u], u3 = jump[u2];
+  int u2 = n[u].jump, u3 = n[u2].jump;
 
-  for (int pos = adj[u]; pos != NIL; pos = e[pos].next) {
+  for (int pos = n[u].adj; pos != NIL; pos = e[pos].next) {
     int v = e[pos].v;
-    if (d[v] == NIL) {
-      d[v] = 1 + d[u];
-      p[v] = u;
-      jump[v] = (d[u2] - d[u] == d[u3] - d[u2]) ? u3 : u;
+    if (n[v].d == NIL) {
+      n[v].d = 1 + n[u].d;
+      n[v].p = u;
+      n[v].jump = (n[u2].d - n[u].d == n[u3].d - n[u2].d) ? u3 : u;
       dfs(v);
     }
   }
 }
 
 int two_ptr_lca(int u, int v) {
-  if (d[v] > d[u]) {
+  if (n[v].d > n[u].d) {
     int tmp = u; u = v; v = tmp;
   }
 
   // First bring them on the same level.
-  while (d[u] > d[v]) {
-    u = (d[jump[u]] >= d[v]) ? jump[u] : p[u];
+  while (n[u].d > n[v].d) {
+    u = (n[n[u].jump].d >= n[v].d) ? n[u].jump : n[u].p;
   }
 
   while (u != v) {
-    if (jump[u] == jump[v]) {
-      u = p[u];
-      v = p[v];
+    if (n[u].jump == n[v].jump) {
+      u = n[u].p;
+      v = n[v].p;
     } else {
-      u = jump[u];
-      v = jump[v];
+      u = n[u].jump;
+      v = n[v].jump;
     }
   }
   return u;
 }
 
 int main() {
-  int n;
+  int nodes;
 
-  scanf("%d", &n);
-  for (int i = 0; i < n; i++) {
-    adj[i] = d[i] = NIL;
+  scanf("%d", &nodes);
+  for (int u = 0; u < nodes; u++) {
+    n[u].adj = n[u].d = NIL;
   }
-  for (int i = 0; i < n - 1; i++) {
+  for (int i = 0; i < nodes - 1; i++) {
     int u, v;
     scanf("%d %d", &u, &v);
     add_edge(u, v, 2 * i);
     add_edge(v, u, 2 * i + 1);
   }
 
-  p[0] = NIL;
-  jump[0] = 0;
-  d[0] = 0;
+  n[0].p = NIL;
+  n[0].jump = 0;
+  n[0].d = 0;
   dfs(0);
 
   int num_queries, u, v;
