7685fc7e77f0ea3cde3a6776c689357f4fa36796
Cătălin Frâncu <cata@francu.com>
2023-02-14 14:19:31 +0200

    Another LCA flavor.

--- trees/lca/two-pointer-ancestor-test.cpp
+++ trees/lca/two-pointer-ancestor-test.cpp
@@ -1,6 +1,7 @@
 // Answer LCA queries via binary lifting with only two pointers. Each node
 // stores two ancestor pointers: one to the parent, one based on this article:
 // https://codeforces.com/blog/entry/74847
+// Uses the ancestor test.
 #include <stdio.h>
 
 #define MAX_N 500'000
@@ -11,11 +12,14 @@ struct edge {
 } e[2 * MAX_N];
 
 struct node {
-  int adj;   // adjacency list
-  int d;     // depth
-  int p;     // parent
-  int jump;  // jump pointer
+  int adj;     // adjacency list
+  int in, out; // discovery / return times
+  int d;       // depth
+  int p;       // parent
+  int jump;    // jump pointer
 } n[MAX_N];
+
+int timer;
 
 void add_edge(int u, int v, int pos) {
   e[pos] = { v, n[u].adj };
@@ -24,7 +28,7 @@ void add_edge(int u, int v, int pos) {
 
 // Traverse the tree and compute parents, depths and jump pointers.
 void dfs(int u) {
-
+  n[u].in = timer++;
   int u2 = n[u].jump, u3 = n[u2].jump;
 
   for (int pos = n[u].adj; pos != NIL; pos = e[pos].next) {
@@ -36,27 +40,24 @@ void dfs(int u) {
       dfs(v);
     }
   }
+  n[u].out = timer++;
+}
+
+// Returns true iff u is an ancestor of v *or* v itself.
+bool is_ancestor(int u, int v) {
+  return (n[u].in <= n[v].in) && (n[u].out >= n[v].out);
 }
 
 int two_ptr_lca(int u, int v) {
-  if (n[v].d > n[u].d) {
-    int tmp = u; u = v; v = tmp;
-  }
-
-  // First bring them on the same level.
-  while (n[u].d > n[v].d) {
-    u = (n[n[u].jump].d >= n[v].d) ? n[u].jump : n[u].p;
-  }
-
-  while (u != v) {
-    if (n[u].jump == n[v].jump) {
+  // Find the lowest ancestor of u that is also an ancestor of v.
+  while (!is_ancestor(u, v)) {
+    if (is_ancestor(n[u].jump, v)) {
       u = n[u].p;
-      v = n[v].p;
     } else {
       u = n[u].jump;
-      v = n[v].jump;
     }
   }
+
   return u;
 }
 
