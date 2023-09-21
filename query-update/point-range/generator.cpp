549aa516a5db18c04fc20464c1766077c46a8e71
Cătălin Frâncu <cata@francu.com>
2023-09-21 11:29:12 +0300

    Refactor generator.

--- query-update/point-range/generator.cpp
+++ query-update/point-range/generator.cpp
@@ -3,80 +3,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <unistd.h>
 
-#define T_UPDATE 1
-#define T_SUM 2
+const int OP_UPDATE = 1;
+const int OP_QUERY = 2;
+
+int size, maxValue, numUpdates, numQueries, delta, p1, p2;
+int root, half;
 
 void usage() {
-  fprintf(stderr, "Usage: generator <n> <m> <q1> <q2> <delta> <p1> <p2>\n");
+  fprintf(stderr, "Usage: generator <size> <maxValue> <numUpdates> <numQueries> <delta> <p1> <p2>\n");
   fprintf(stderr, "\n");
-  fprintf(stderr, "Generates an array of n random integers in [0, m).\n");
-  fprintf(stderr, "Then generates q1 updates and q2 queries as follows:\n");
+  fprintf(stderr, "Generates an array of n random integers in [0, maxValue).\n");
+  fprintf(stderr, "Then generates numUpdates updates and numQueries queries as follows:\n");
   fprintf(stderr, "* p1%% of the queries will be smaller than sqrt(n)\n");
   fprintf(stderr, "* p2%% of the queries will be between sqrt(n) and n/2\n");
   fprintf(stderr, "* the remaining queries will be larger than n/2\n");
   fprintf(stderr, "Updates will be between -delta and +delta.\n");
   fprintf(stderr, "All queries are [closed,closed] and all indices are 1-based.\n");
-  _exit(1);
+  exit(1);
+}
+
+void parseCommandLineArgs(int argc, char** argv) {
+  if (argc != 8) {
+    usage();
+  }
+
+  size = atoi(argv[1]);
+  maxValue = atoi(argv[2]);
+  numUpdates = atoi(argv[3]);
+  numQueries = atoi(argv[4]);
+  delta = atoi(argv[5]);
+  p1 = atoi(argv[6]);
+  p2 = atoi(argv[7]);
+}
+
+void computeOtherValues() {
+  root = sqrt(size);
+  half = size / 2;
+}
+
+void initRng() {
+  struct timeval time;
+  gettimeofday(&time, NULL);
+  srand(time.tv_usec);
 }
 
 int rand2(int x, int y) {
   return rand() % (y - x + 1) + x;
 }
 
-int main(int argc, char** argv) {
-  if (argc != 8) {
-    usage();
-  }
-
-  int n = atoi(argv[1]);
-  int m = atoi(argv[2]);
-  int q[3] = { 0, atoi(argv[3]), atoi(argv[4]) };
-  int delta = atoi(argv[5]);
-  int p1 = atoi(argv[6]);
-  int p2 = atoi(argv[7]);
-
-  struct timeval time;
-  gettimeofday(&time, NULL);
-  srand(time.tv_usec);
-
-  printf("%d %d\n", n, q[1] + q[2]);
-  for (int i = 0; i < n; i++) {
-    printf("%d ", rand() % m);
+void emitArray() {
+  printf("%d %d\n", size, numUpdates + numQueries);
+  for (int i = 0; i < size; i++) {
+    printf("%d ", rand() % maxValue);
   }
   printf("\n");
+}
 
-  int sq = sqrt(n), half = n / 2;
+int pickOp() {
+  int numOps = numUpdates + numQueries;
+  return (rand() % numOps < numUpdates)
+    ? OP_UPDATE
+    : OP_QUERY;
+}
 
-  while (q[1] + q[2])  {
-    int t = (rand() % (q[1] + q[2]) < q[1]) ? 1 : 2;
-    q[t]--;
+void emitUpdate() {
+  int pos = rand2(1, size);
+  int val = rand2(-delta, delta);
+  printf("%d %d %d\n", OP_UPDATE, pos, val);
+  numUpdates--;
+}
 
-    if (t == T_UPDATE) {
-
-      int pos = rand2(1, n);
-      int val = rand2(-delta, delta);
-      printf("%d %d %d\n", t, pos, val);
-
-    }  else {
-
-      int p = rand() % 100, size;
-      if (p < p1) {
-        size = rand2(1, sq - 1);
-      } else if (p < p1 + p2) {
-        size = rand2(sq, half - 1);
-      } else {
-        size = rand2(half, n);
-      }
-
-      int l = rand2(1, n - size + 1), r = l + size - 1;
-      assert((0 <= l) && (l <= r) && (r <= n));
-
-      printf("%d %d %d\n", t, l, r);
-    }
-
+int pickQueryLen() {
+  int p = rand() % 100;
+  if (p < p1) {
+    return rand2(1, root - 1);
+  } else if (p < p1 + p2) {
+    return rand2(root, half - 1);
+  } else {
+    return rand2(half, size);
   }
+}
+
+void emitQuery() {
+  int len = pickQueryLen();
+  int l = rand2(1, size - len + 1);
+  int r = l + len - 1;
+  assert((1 <= l) && (l <= r) && (r <= size));
+
+  printf("%d %d %d\n", OP_QUERY, l, r);
+  numQueries--;
+}
+
+void emitOps() {
+  while (numUpdates + numQueries)  {
+    int op = pickOp();
+
+    if (op == OP_UPDATE) {
+      emitUpdate();
+    } else {
+      emitQuery();
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  parseCommandLineArgs(argc, argv);
+  computeOtherValues();
+  initRng();
+
+  emitArray();
+  emitOps();
 
   return 0;
 }
