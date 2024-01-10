0e2cea62a47178ca1fe50d0b66ea0353a028cb0e
Cătălin Frâncu <cata@francu.com>
2024-01-10 12:21:11 +0200

    Tweaks to the permutation benchmark.

--- combinatorics/permutation-rank.cpp
+++ combinatorics/permutation-rank.cpp
@@ -1,34 +1,79 @@
+// Remember that _pdep_u32 requires the -march=native compilation flag.
+//
 // Permutation ranking and unranking using several methods. None of them are
 // exceedingly fancy; we want them to be reasonably easy to code.
 #include <assert.h>
+#include <immintrin.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 
-// We'll rank and unrank TRIALS random permutations of size N.
-#define TRIALS 10'000'000
-#define N 20
-#define MAX_P2 16 // for fenwick_bin_search()
+// We'll rank and unrank NUM_PASSES * NUM_PERMS random permutations of size N.
+const int NUM_PERMS = 1'000;
+const int NUM_PASSES = 10'000;
+const int N = 20;
+const int MAX_P2 = 16; // for fenwick_bin_search()
 
 typedef unsigned long long u64;
 
 typedef struct {
-  u64 rank;
-  char v[N];
+  u64 rank, witness;
+  int v[N];
 } permutation;
 
-permutation p[TRIALS];
+permutation p[NUM_PERMS], q[NUM_PERMS];
 u64 fact[N];
 char kth[1 << N][N]; // kth[x][k] = the k-th UNSET bit in x (0-based)
 
-int min(int x, int y) {
-  return (x < y) ? x : y;
+void generate_permutations() {
+  for (int i = 0; i < NUM_PERMS; i++) {
+    for (int j = 0; j < N; j++) {
+      int k = rand() % (j + 1);
+      p[i].v[j] = p[i].v[k];
+      p[i].v[k] = j;
+    }
+  }
+}
+
+void precompute_factorials() {
+  fact[0] = 1;
+  for (int i = 1; i < N; i++) {
+    fact[i] = fact[i - 1] * i;
+  }
+}
+
+void precompute_kth() {
+  for (int i = 0; i < (1 << N); i++) {
+    int cnt = 0;
+    for (int k = 0; k < N; k++) {
+      if ((i & (1 << k)) == 0) {
+        kth[i][cnt++] = k;
+      }
+    }
+    // Values from cnt to N are meaningless because there aren't enough unset
+    // bits in i. Either set them to a special value or make sure the code
+    // doesn't read those values.
+  }
+}
+
+void verify_rank() {
+  for (int i = 0; i < NUM_PERMS; i++) {
+    assert(p[i].rank == p[i].witness);
+  }
+}
+
+void verify_unrank() {
+  for (int i = 0; i < NUM_PERMS; i++) {
+    for (int j = 0; j < N; j++) {
+      assert(p[i].v[j] == q[i].v[j]);
+    }
+  }
 }
 
 /****************************************************************
  *                       Time management                        *
  ****************************************************************/
-long long __time1;
+long long t0;
 
 long long get_time() {
   struct timeval time;
@@ -37,12 +82,15 @@ long long get_time() {
 }
 
 void mark_time() {
-  __time1 = get_time();
+  t0 = get_time();
 }
 
 void report_time(const char* msg) {
-  long long time2 = get_time();
-  fprintf(stderr, "%s: %lld milliseconds\n", msg, time2 - __time1);
+  int millis = get_time() - t0;
+  int ops = NUM_PASSES * NUM_PERMS;
+
+  printf("%s: %d ms pentru %d operații (%d Mops/sec)\n",
+         msg, millis, ops, ops / millis / 1000);
 }
 
 /****************************************************************
@@ -51,33 +99,42 @@ void report_time(const char* msg) {
 
 bool used[N];
 
-u64 rank_naive(char* p) {
-  u64 result = 0;
-  for (int i = 0; i < N; i++) {
-    // count the elements smaller than p[i]
-    int cnt = 0;
-    for (int j = 0; j < i; j++) {
-      cnt += (p[j] < p[i]);
+void rank_naive() {
+  for (int pass = 0; pass < NUM_PASSES; pass++) {
+    for (int k = 0; k < NUM_PERMS; k++) {
+      u64 rank = 0;
+      for (int i = 0; i < N; i++) {
+        // count the elements smaller than p[i]
+        int cnt = 0;
+        for (int j = 0; j < i; j++) {
+          cnt += (p[k].v[j] < p[k].v[i]);
+        }
+        rank += fact[N - 1 - i] * (p[k].v[i] - cnt);
+      }
+      p[k].rank = p[k].witness = rank;
     }
-    result += fact[N - 1 - i] * (p[i] - cnt);
   }
-  return result;
 }
 
-void unrank_naive(u64 rank, char* witness) {
-  for (int i = 0; i < N; i++) {
-    used[i] = false;
-  }
-  for (int i = 0; i < N; i++) {
-    int q = rank / fact[N - 1 - i];
-    rank %= fact[N - 1 - i];
-    // get the q-th unused value
-    int j = 0;
-    while (q || used[j]) {
-      q -= !used[j++];
+void unrank_naive() {
+  for (int pass = 0; pass < NUM_PASSES; pass++) {
+    for (int k = 0; k < NUM_PERMS; k++) {
+      for (int i = 0; i < N; i++) {
+        used[i] = false;
+      }
+      u64 rank = p[k].rank;
+      for (int i = 0; i < N; i++) {
+        int quot = rank / fact[N - 1 - i];
+        rank %= fact[N - 1 - i];
+        // get the quot-th unused value
+        int j = 0;
+        while (quot || used[j]) {
+          quot -= !used[j++];
+        }
+        q[k].v[i] = j;
+        used[j] = true;
+      }
     }
-    assert(j == witness[i]);
-    used[j] = true;
   }
 }
 
@@ -138,28 +195,37 @@ int fenwick_bin_search(int val) {
   return pos;
 }
 
-u64 rank_fenwick(char* p) {
-  fenwick_clear();
-  u64 result = 0;
-  for (int i = 0; i < N; i++) {
-    // count the elements smaller than p[i] -- same as naive version
-    int cnt = fenwick_sum(p[i] + 1);
-    result += fact[N - 1 - i] * (p[i] - cnt);
-    fenwick_add(p[i] + 1, 1);
+void rank_fenwick() {
+  for (int pass = 0; pass < NUM_PASSES; pass++) {
+    for (int k = 0; k < NUM_PERMS; k++) {
+      fenwick_clear();
+      u64 rank = 0;
+      for (int i = 0; i < N; i++) {
+        // count the elements smaller than p[i] -- same as naive version
+        int cnt = fenwick_sum(p[k].v[i] + 1);
+        rank += fact[N - 1 - i] * (p[k].v[i] - cnt);
+        fenwick_add(p[k].v[i] + 1, 1);
+      }
+      p[k].rank = rank;
+    }
   }
-  return result;
 }
 
-void unrank_fenwick(u64 rank, char* witness) {
-  // Fenwick values of 1 indicate still usable elements
-  fenwick_set_1();
-  for (int i = 0; i < N; i++) {
-    int q = rank / fact[N - 1 - i];
-    rank %= fact[N - 1 - i];
-    // get the q-th usable value
-    int j = fenwick_bin_search(q + 1);
-    assert(j == witness[i]);
-    fenwick_add(j + 1, -1);
+void unrank_fenwick() {
+  for (int pass = 0; pass < NUM_PASSES; pass++) {
+    for (int k = 0; k < NUM_PERMS; k++) {
+      u64 rank = p[k].rank;
+      // Fenwick values of 1 indicate still usable elements
+      fenwick_set_1();
+      for (int i = 0; i < N; i++) {
+        int quot = rank / fact[N - 1 - i];
+        rank %= fact[N - 1 - i];
+        // get the quot-th usable value
+        int j = fenwick_bin_search(quot + 1);
+        q[k].v[i] = j;
+        fenwick_add(j + 1, -1);
+      }
+    }
   }
 }
 
@@ -167,69 +233,56 @@ void unrank_fenwick(u64 rank, char* witness) {
  *                           Popcount                           *
  ****************************************************************/
 
-u64 rank_popcount(char* p) {
-  u64 result = 0;
-  int mask = 0;
-  for (int i = 0; i < N; i++) {
-    // count the number of set bits among bits [0...p[i])
-    int cnt = __builtin_popcount(mask & ((1 << p[i]) - 1));
-    result += fact[N - 1 - i] * (p[i] - cnt);
-    mask |= (1 << p[i]);
-  }
-  return result;
-}
-
-void unrank_popcount(u64 rank, char* witness) {
-  int mask = 0;
-  for (int i = 0; i < N; i++) {
-    int q = rank / fact[N - 1 - i];
-    rank %= fact[N - 1 - i];
-    // get the q-th unused value
-    int j = kth[mask][q];
-    assert(j == witness[i]);
-    mask |= (1 << j);
-  }
-}
-
-void unrank_popcount2(u64 rank, char* witness) {
-  int mask = 0;
-  for (int i = 0; i < N; i++) {
-    int q = rank / fact[N - 1 - i];
-    rank %= fact[N - 1 - i];
-
-    // Get the q-th unused value. Same as above, but we check the lower half
-    // of the mask, then the higher half. The point is that we only need 2^10
-    // entries instead of 2^20, which runs much faster because of better
-    // caching.
-    int lo = mask & ((1 << (N >> 1)) - 1);
-    int pop = (N >> 1) - __builtin_popcount(lo);
-    int j = (q < pop)
-      ? kth[lo][q]
-      : ((N >> 1) + kth[mask >> (N >> 1)][q - pop]);
-
-    assert(j == witness[i]);
-    mask |= (1 << j);
-  }
-}
-
-// Same as unrank_popcount2, but replaces divisions with repeated subtractions.
-void unrank_popcount3(u64 rank, char* witness) {
-  int mask = 0;
-  for (int i = 0; i < N; i++) {
-    int q = 0;
-    while (fact[N - 1 - i] <= rank) {
-      rank -= fact[N - 1 - i];
-      q++;
+void rank_popcount() {
+  for (int pass = 0; pass < NUM_PASSES; pass++) {
+    for (int k = 0; k < NUM_PERMS; k++) {
+      u64 rank = 0;
+      int mask = 0;
+      for (int i = 0; i < N; i++) {
+        // count the number of set bits among bits [0...p[i])
+        int cnt = __builtin_popcount(mask & ((1 << p[k].v[i]) - 1));
+        rank += fact[N - 1 - i] * (p[k].v[i] - cnt);
+        mask |= (1 << p[k].v[i]);
+      }
+      p[k].rank = rank;
     }
+  }
+}
 
-    int lo = mask & ((1 << (N >> 1)) - 1);
-    int pop = (N >> 1) - __builtin_popcount(lo);
-    int j = (q < pop)
-      ? kth[lo][q]
-      : ((N >> 1) + kth[mask >> (N >> 1)][q - pop]);
+void unrank_popcount() {
+  for (int pass = 0; pass < NUM_PASSES; pass++) {
+    for (int k = 0; k < NUM_PERMS; k++) {
+      u64 rank = p[k].rank;
+      int mask = 0;
+      for (int i = 0; i < N; i++) {
+        int quot = rank / fact[N - 1 - i];
+        rank %= fact[N - 1 - i];
+        // get the quot-th unused value
+        int j = kth[mask][quot];
+        q[k].v[i] = j;
+        mask |= (1 << j);
+      }
+    }
+  }
+}
 
-    assert(j == witness[i]);
-    mask |= (1 << j);
+void unrank_popcount2() {
+  for (int pass = 0; pass < NUM_PASSES; pass++) {
+    for (int k = 0; k < NUM_PERMS; k++) {
+      u64 rank = p[k].rank;
+      int mask = 0;
+      for (int i = 0; i < N; i++) {
+        int quot = rank / fact[N - 1 - i];
+        rank %= fact[N - 1 - i];
+
+        // get the quot-th unused value
+        int zero_bit_mask = _pdep_u32(1 << quot, ~mask);
+        int j = _tzcnt_u32(zero_bit_mask);
+
+        q[k].v[i] = j;
+        mask |= (1 << j);
+      }
+    }
   }
 }
 
@@ -238,88 +291,49 @@ int main() {
   gettimeofday(&time, NULL);
   srand(time.tv_usec);
 
-  // precompute factorials
-  fact[0] = 1;
-  for (int i = 1; i < N; i++) {
-    fact[i] = fact[i - 1] * i;
-  }
-
-  // precompute k-th bit values
   mark_time();
-  for (int i = 0; i < (1 << N); i++) {
-    int cnt = 0;
-    for (int k = 0; k < N; k++) {
-      if (!(i & (1 << k))) {
-        kth[i][cnt++] = k;
-      }
-    }
-    // Values from cnt to N are meaning less because there aren't enough unset
-    // bits in i. Either set them to a special value or make sure the code
-    // doesn't read those values.
-  }
-  report_time("Precomputed k-th set bit values");
-
-  // generate the trials
-  mark_time();
-  for (int i = 0; i < TRIALS; i++) {
-    for (int j = 0; j < N; j++) {
-      int k = rand() % (j + 1);
-      p[i].v[j] = p[i].v[k];
-      p[i].v[k] = j;
-    }
-    p[i].rank = rank_popcount(p[i].v);
-  }
-  report_time("Generated trials");
+  precompute_factorials();
+  precompute_kth();
+  generate_permutations();
+  rank_naive();
+  report_time("inițializare");
   printf("--\n");
 
   mark_time();
-  for (int i = 0; i < TRIALS; i++) {
-    assert(rank_naive(p[i].v) == p[i].rank);
-  }
-  report_time("Naive rank");
+  rank_naive();
+  report_time("rank naiv");
+  verify_rank();
 
   mark_time();
-  for (int i = 0; i < TRIALS; i++) {
-    assert(rank_fenwick(p[i].v) == p[i].rank);
-  }
-  report_time("Fenwick rank");
+  rank_fenwick();
+  report_time("rank cu AIB");
+  verify_rank();
 
   mark_time();
-  for (int i = 0; i < TRIALS; i++) {
-    assert(rank_popcount(p[i].v) == p[i].rank);
-  }
-  report_time("Popcount rank");
+  rank_popcount();
+  report_time("rank cu popcount");
+  verify_rank();
   printf("--\n");
 
   mark_time();
-  for (int i = 0; i < TRIALS; i++) {
-    unrank_naive(p[i].rank, p[i].v);
-  }
-  report_time("Naive unrank");
+  unrank_naive();
+  report_time("unrank naiv");
+  verify_unrank();
 
   mark_time();
-  for (int i = 0; i < TRIALS; i++) {
-    unrank_fenwick(p[i].rank, p[i].v);
-  }
-  report_time("Fenwick unrank");
+  unrank_fenwick();
+  report_time("unrank cu AIB");
+  verify_unrank();
 
   mark_time();
-  for (int i = 0; i < TRIALS; i++) {
-    unrank_popcount(p[i].rank, p[i].v);
-  }
-  report_time("Popcount unrank");
+  unrank_popcount();
+  report_time("unrank cu popcount și tabel");
+  verify_unrank();
 
   mark_time();
-  for (int i = 0; i < TRIALS; i++) {
-    unrank_popcount2(p[i].rank, p[i].v);
-  }
-  report_time("Popcount2 unrank");
-
-  mark_time();
-  for (int i = 0; i < TRIALS; i++) {
-    unrank_popcount3(p[i].rank, p[i].v);
-  }
-  report_time("Popcount3 unrank");
+  unrank_popcount2();
+  report_time("unrank cu popcount și pdep");
+  verify_unrank();
 
   return 0;
 }
